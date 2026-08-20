f2f86bc5fdd517f65e5b5d22794205e144ee0786
sakura0925wsq111-cpu <sakura.0925wsq111@gmail.com>
2026-08-20 14:31:05 +0800

    Keep master backend-only

--- miniprogram/utils/date.js
+++
@@ -1,9 +0,0 @@
-function normalizeDateStr(year, month, day) {
-  const date = new Date(Number(year), Number(month) - 1, Number(day));
-  const y = date.getFullYear();
-  const m = String(date.getMonth() + 1).padStart(2, "0");
-  const d = String(date.getDate()).padStart(2, "0");
-  return y + "-" + m + "-" + d;
-}
-
-module.exports = { normalizeDateStr };
